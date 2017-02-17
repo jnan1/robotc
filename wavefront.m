a7a5f3a3654b82f8364b74f5de6575ab5c48d720
jnan1 <jnan1@andrew.cmu.edu>
2017-02-17 17:33:49 -0500

    8-way conn

--- wavefront.m
+++ wavefront.m
@@ -52,33 +52,41 @@ path = cat(3,im.*255,im.*255,im.*255);
 while ~(current(1,1) == endx && current(1,2) == endy)
     x = current(1,1);
     y = current(1,2);
-    if(x < m && image(x+1, y) ~= 1 && image(x+1,y) < image(x,y))
-        arr(counter, :) = [x+1,y];
-        path(x+1, y, 1) = 255;
-    elseif(x > 1 && image(x-1, y) ~= 1 && image(x-1,y) < image(x,y))
-        arr(counter,:) = [x-1,y];
-        path(x-1, y, 1) = 255;
-    elseif(y < n && image(x, y+1) ~= 1 && image(x,y+1) < image(x,y))
-        arr(counter,:) = [x,y+1];
-        path(x, y+1, 1) = 255;
-    elseif(y > 1 && image(x, y-1) ~= 1 && image(x,y-1) < image(x,y))
-        arr(counter,:) = [x,y-1];
-        path(x, y-1, 1) = 255;
-    elseif(x >1 && y > 1 && image(x-1, y-1) ~= 1 && image(x-1,y-1) < image(x,y))
-        arr(counter,:) = [x-1,y-1];
-        path(x-1, y-1, 1) = 255;
-    elseif(x >1 && y < n && image(x-1, y+1) ~= 1 && image(x-1,y+1) < image(x,y))
-        arr(counter,:) = [x-1,y+1];
-        path(x-1, y+1, 1) = 255;
-    elseif(x<m && y > 1 && image(x+1, y-1) ~= 1 && image(x+1,y-1) < image(x,y))
-        arr(counter,:) = [x+1,y-1];
-        path(x+1, y-1, 1) = 255;
-    elseif(x<m && y< n && image(x+1, y+1) ~= 1 && image(x+1,y+1) < image(x,y))
-        arr(counter,:) = [x+1,y+1];
-        path(x+1, y+1, 1) = 255;
+    b = zeros(1,8);
+    b(1,1) = x < m && image(x+1, y) ~= 1 && image(x+1,y) < image(x,y);
+    b(1,2) = x > 1 && image(x-1, y) ~= 1 && image(x-1,y) < image(x,y);
+    b(1,3) = y < n && image(x, y+1) ~= 1 && image(x,y+1) < image(x,y);
+    b(1,4) = y > 1 && image(x, y-1) ~= 1 && image(x,y-1) < image(x,y);
+    b(1,5) = x > 1 && y > 1 && image(x-1, y-1) ~= 1 && image(x-1,y-1) < image(x,y);
+    b(1,6) = x > 1 && y < n && image(x-1, y+1) ~= 1 && image(x-1,y+1) < image(x,y);
+    b(1,7) = x < m && y > 1 && image(x+1, y-1) ~= 1 && image(x+1,y-1) < image(x,y);
+    b(1,8) = x<m && y< n && image(x+1, y+1) ~= 1 && image(x+1,y+1) < image(x,y);
+    pts = zeros(8,2);
+    pts(1,:) = [x+1, y];
+    pts(2,:) = [x-1, y];
+    pts(3,:) = [x,y+1];
+    pts(4,:) = [x,y-1];
+    pts(5,:) = [x-1,y-1];
+    pts(6,:) = [x-1,y+1];
+    pts(7,:) = [x+1,y-1];
+    pts(8,:) = [x+1,y+1];
+    if(b(1,defaultdir))
+        arr(counter,:) = pts(defaultdir,:);
+        current = arr(counter,:);
+        counter = counter + 1;
+        path(pts(defaultdir,1),pts(defaultdir,2),1) = 255;
+        continue;
     end
-    current = arr(counter,:);
-    counter = counter + 1;
+    for i = 1 : 8
+        if(b(1,i))
+            arr(counter,:) = pts(i,:);
+            current = arr(counter,:);
+            counter = counter + 1;
+            path(pts(i,1),pts(i,2),1) = 255;
+            defaultdir = i;
+            break;
+        end
+    end
 end
 
 % size(path)
